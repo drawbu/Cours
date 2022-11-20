2458ecc7cd103b7cc97268078f0192fedb911d51
drawbu <69208565+drawbu@users.noreply.github.com>
2022-11-20 14:56:41 +0100

    :sparkles: Add level of color and complexity exercice, for TD machine 3

--- TD_machine_3/TD_machine_3.py
+++ TD_machine_3/TD_machine_3.py
@@ -14,5 +14,74 @@ def coloured_stripes(img: Image) -> Image:
             (84, 34, 221), (240, 153, 146), (228, 235, 244), (82, 223, 217),
             (222, 152, 14), (176, 128, 72)
     )):
-        solid_rectangle(img, x * (img.size[0] // 10), (x + 1) * (img.size[0] // 10), 0, img.size[1], color)
+        solid_rectangle(
+            img,
+            x * (img.size[0] // 10),
+            (x + 1) * (img.size[0] // 10),
+            0,
+            img.size[1],
+            color
+        )
     return img
+
+
+def minimum_colors(img: Image) -> list[int, int, int]:
+    minimum = [255, 255, 255]
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            for i in range(len(minimum)):
+                if minimum[i] > img.getpixel((x, y))[i]:
+                    minimum[i] = img.getpixel((x, y))[i]
+    return minimum
+
+
+def maximum_colors(img: Image) -> list[int, int, int]:
+    maximum = [0, 0, 0]
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            for i in range(len(maximum)):
+                if maximum[i] < img.getpixel((x, y))[i]:
+                    maximum[i] = img.getpixel((x, y))[i]
+    return maximum
+
+
+def color_vector(color, minimum, maximum) -> tuple[int, int, int]:
+    return (
+        (255 * (color[0] - minimum[0])) // (maximum[0] - minimum[0]),
+        (255 * (color[1] - minimum[1])) // (maximum[1] - minimum[1]),
+        (255 * (color[2] - minimum[2])) // (maximum[2] - minimum[2]),
+    )
+
+
+def renew_color_levels(img: Image):
+    maximum = maximum_colors(img)
+    minimum = minimum_colors(img)
+    for x in range(img.size[0]):
+        for y in range(img.size[0]):
+            color = img.getpixel((x, y))
+            img.putpixel(
+                (x, y),
+                color_vector(color, minimum, maximum)
+            )
+    return img
+
+
+# Tests
+def tests() -> None:
+    img1 = Image.new("RGB", (300, 300))
+    img2 = coloured_stripes(Image.new("RGB", (300, 300)))
+
+    # minimum_colors
+    assert minimum_colors(img1) == [0, 0, 0]
+    assert minimum_colors(img2) == [18, 34, 14]
+
+    # maximum_colors
+    assert maximum_colors(img1) == [0, 0, 0]
+    assert maximum_colors(img2) == [240, 235, 244]
+
+
+if __name__ == "__main__":
+    img = Image.new("RGB", (300, 300))
+    img = coloured_stripes(img)
+    img = renew_color_levels(img)
+    img.show()
