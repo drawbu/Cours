d1b775da73f893c9288f61f4c70bc90b6ead3d29
drawbu <69208565+drawbu@users.noreply.github.com>
2022-11-21 20:13:25 +0100

    :sparkles: Add egyptian fraction exercice for TP2

--- TP2/2-egyptian_fraction.py
+++ TP2/2-egyptian_fraction.py
@@ -1,14 +1,20 @@
 from array import array
 
 
-def fraction_egyptienne2(a, b, t, i=0):
-    ...
+def egyptian_fraction2(a: int, b: int, t: array, i: int = 0) -> int:
+    if a == 1:
+        t[i] = b
+        return i
+    i = egyptian_fraction2(1, b, t, i)
+    i = egyptian_fraction2(a - 1, b + 1, t, i + 1)
+    i = egyptian_fraction2(a - 1, b * (b + 1), t, i + 1)
+    return i
 
 
 def tests():
-    print("\nRetour sur la fraction égyptienne")
-    print(
-        f"  fraction_egyptienne2(3, 5, [0]*10, 0): {fraction_egyptienne2(3, 5, array('l', [0] * 10))}")
+    tab = array("l", [0] * 10)
+    egyptian_fraction2(3, 5, tab)
+    assert tab == array("l", [5, 6, 7, 42, 30, 31, 930, 0, 0, 0])
 
 
 if __name__ == "__main__":
