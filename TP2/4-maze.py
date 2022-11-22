3f4862dc6647aba70d0580446a55a0c28ee3603b
drawbu <69208565+drawbu@users.noreply.github.com>
2022-11-22 09:48:36 +0100

    :sparkles: Add maze exercice for TP2

--- TP2/4-maze.py
+++ TP2/4-maze.py
@@ -1,14 +1,49 @@
-class Labyrinthe:
-    def __init__(self):
-        self.labyrinthe = np.array([[1] * 4] * 4)
-        self.labyrinthe[2][0] = 0
-        self.labyrinthe[1][1] = 0
-        print(self.look_neighbourhood(2, 2))
+from pprint import pprint
 
-    def look_neighbourhood(self, x, y):
-        for neighbour_x in range(4):
-            for neighbour_y in range(4):
-                print(neighbour_x, neighbour_y)
+Grid = list[list[int]]
+Point = tuple[int, int]
 
-    def __repr__(self):
-        return "\n" + "\n".join(" ".join(map(str, e)) for e in self.labyrinthe)
+solutions = []
+
+
+def maze(grid: Grid, n: int, start: Point = None, moves: list = None):
+    global solutions
+    if moves is None:
+        moves = []
+    if start is None:
+        start = (0, 0)
+        solutions = []
+
+    if start == (n - 1, n - 1):
+        solutions.append(moves)
+        return solutions
+
+    if start[0] + 1 < n:
+        next_point: Point = (start[0] + 1, start[1])
+        if grid[next_point[0]][next_point[1]] == 1:
+            maze(grid, n, next_point, moves + [next_point])
+
+    if start[1] + 1 < n:
+        next_point: Point = (start[0], start[1] + 1)
+        if grid[next_point[0]][next_point[1]] == 1:
+            maze(grid, n, next_point, moves + [next_point])
+    return solutions
+
+
+def tests():
+    maze_grid: Grid = [
+        [1, 1, 1, 1],
+        [1, 0, 1, 1],
+        [0, 1, 1, 1],
+        [1, 1, 1, 1],
+    ]
+    assert maze(maze_grid, 4) == [
+        [(0, 1), (0, 2), (1, 2), (2, 2), (3, 2), (3, 3)],
+        [(0, 1), (0, 2), (1, 2), (2, 2), (2, 3), (3, 3)],
+        [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 3)],
+        [(0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (3, 3)],
+    ]
+
+
+if __name__ == "__main__":
+    tests()
