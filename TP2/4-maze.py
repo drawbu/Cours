91c876770761b64cb998870fdb2a3356a9af10b0
drawbu <69208565+drawbu@users.noreply.github.com>
2022-11-22 19:22:05 +0100

    :recycle: Add parameter to avoid global

--- TP2/4-maze.py
+++ TP2/4-maze.py
@@ -1,36 +1,38 @@
-from pprint import pprint
-
 Grid = list[list[int]]
 Point = tuple[int, int]
 
-solutions = []
 
-
-def maze(grid: Grid, n: int, start: Point = None, moves: list = None):
-    global solutions
-    if moves is None:
-        moves = []
+def maze(
+        grid: Grid,
+        n: int,
+        start: Point = None,
+        path: list[Point] = None,
+        paths: list[list[Point]] = None
+):
+    if path is None:
+        path = []
+    if paths is None:
+        paths = []
     if start is None:
         start = (0, 0)
-        solutions = []
 
     if start == (n - 1, n - 1):
-        solutions.append(moves)
-        return solutions
+        paths.append(path)
+        return paths
 
     if start[0] + 1 < n:
         next_point: Point = (start[0] + 1, start[1])
         if grid[next_point[0]][next_point[1]] == 1:
-            maze(grid, n, next_point, moves + [next_point])
+            maze(grid, n, next_point, path + [next_point], paths)
 
     if start[1] + 1 < n:
         next_point: Point = (start[0], start[1] + 1)
         if grid[next_point[0]][next_point[1]] == 1:
-            maze(grid, n, next_point, moves + [next_point])
-    return solutions
+            maze(grid, n, next_point, path + [next_point], paths)
+    return paths
 
 
-def tests():
+def test_maze():
     maze_grid: Grid = [
         [1, 1, 1, 1],
         [1, 0, 1, 1],
@@ -43,7 +45,3 @@ def tests():
         [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 3)],
         [(0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (3, 3)],
     ]
-
-
-if __name__ == "__main__":
-    tests()
